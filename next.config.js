b809274d9fddee24bbb545dbfc98cfd3f3275486
fireMan-34 <524915379@qq.com>
2024-04-26 08:59:45 +0800

    ✨ feat: wai test next 12.3.4

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    return config;
+  },
+  compiler: {
+    
+  },
+  experimental: {
+    // browsersListForSwc: true,
+    // legacyBrowsers: true,
+  }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <button onClick={function(){ console.log('ok') }} >go</button>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
