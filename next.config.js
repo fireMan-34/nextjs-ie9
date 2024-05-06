e173b1d73a43c1a712a6b4e1604f78caa034eacb
maxufeng <unf-apqb330d4@dingtalk.com>
2024-05-06 16:33:45 +0800

    feat: 输出 webpack 配置

--- next.config.js
+++ next.config.js
@@ -1,16 +1,51 @@
+const { writeFile } = require('fs/promises');
+const path = require('path');
+
+/**
+ * out webpack config for debuging config work
+ * @param {*} config 
+ * @param {import('next/dist/server/config-shared').WebpackConfigContext} options 
+ */
+function webpackConfigOuter(config, options) {
+  const nextDirPath = path.join(__dirname, '.next');
+
+  let jsonObj = {
+    config,
+    options,
+  };
+
+  jsonObj = JSON.stringify(jsonObj, (key, value) => {
+    if (typeof value === 'bigint') {
+      return value.toString();
+    } else if (typeof value === 'function') {
+      return value.toString();
+    }
+    return value;
+  }, 2);
+
+  writeFile(path.join(nextDirPath, `config.${options.isServer ? 'server' : 'client'}.json`), jsonObj, 'utf-8');
+}
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
+  webpack: (config, options) => {
+
+    config.resolve.alias.compoents = path.resolve(__dirname, 'components');
+    config.resolve.alias.polyfills = path.resolve(__dirname, 'polyfills');
+
+    webpackConfigOuter(config,options);
+
     return config;
   },
   compiler: {
-    
+
   },
   experimental: {
     // browsersListForSwc: true,
-    // legacyBrowsers: true,
+    legacyBrowsers: true,
   }
 }
 
