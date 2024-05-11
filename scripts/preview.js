11f80d8c19f62d357253954c1a8018529975548d
maxufeng <unf-apqb330d4@dingtalk.com>
2024-05-11 18:03:54 +0800

    feat: add browser env

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,17 +1,20 @@
+
 try {
+  const { join } = require('path');
   const { execSync } = require('child_process');
-  const chalk = require('chalk');
+  const { loadEnvConfig, } = require('@next/env');
+
+  loadEnvConfig(join(__dirname, './preview'));
 
   const root = process.cwd();
 
-  execSync('pwsh.exe --Command \"if (Test-Path ./.next) { Remove-Item -Path ./.next -Recurse -Force }\" ', { cwd: root, stdio: [process.stdin, process.stdout, process.stderr] });
-  execSync("yarn cross-env NODE_ENV=preview && yarn nodemon --config ./preview.nodemon.json", { cwd: root, stdio: [process.stdin, process.stdout, process.stderr] });
+  execSync("yarn nodemon --config ./preview.nodemon.json", { cwd: root, stdio: [process.stdin, process.stdout, process.stderr] });
 
 } catch (error) {
-  console.log(`catcher ${chalk.red(__filename)} error`);
+  console.log(`catcher ${'__filename'} error`);
   console.error(error);
   process.exit(1)
 } finally {
-  console.log(chalk.blue('next12 ie9+'), 'support is running');
+  console.log('next12 ie9+', 'support is running');
 }
 
