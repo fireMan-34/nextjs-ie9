12d3e812c92d631ccd50336cab9c228fa1c190f5
maxufeng <unf-apqb330d4@dingtalk.com>
2024-05-22 10:24:05 +0800

    feat: i18n varibale and dev refresh

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -5,5 +5,11 @@ module.exports = {
   i18n: {
     defaultLocale: 'zh_CN',
     locales: [ 'en_US', 'zh_CN' ],
-  }
+  },
+  fallbackLng: {
+    default: [ 'zh_CN' ],
+  },
+  // 用于开发环境热重载国际化 map ，对性能有一定影响
+  // https://github.com/i18next/next-i18next/issues/2109
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
